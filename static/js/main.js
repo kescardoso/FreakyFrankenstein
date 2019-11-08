f2f0d6302ef3f08829cf57367d04ed5810474800
Emil <emil@emilohlund.se>
2019-11-08 17:07:08 +0100

    Added pumpkin functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
-function run() {
-    console.log("running");
-}
+let pumpkins = document.querySelectorAll(".pumpkin");
+
+const pumpkinImages = {
+  whole: "url(../../images/pumpkins/pumpkin_whole.png)",
+  halfSmashed: "url(../../images/pumpkins/pumpkin_half_smashed.png)",
+  smashed: "url(../../images/pumpkins/pumpkin_smashed.png)",
+};
+
+pumpkins.forEach(function(pumpkin) {
+  let smashed = false;
+  pumpkin.addEventListener("click", function() {
+    if (!smashed) {
+      this.style.backgroundImage = pumpkinImages.halfSmashed;
+      setTimeout(function() {
+        pumpkin.style.backgroundImage = pumpkinImages.smashed;
+        smashed = true;
+      }, 150);
+    }
+  });
+});
